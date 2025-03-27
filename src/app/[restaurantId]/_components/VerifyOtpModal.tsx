baa1e43d0f8ad189f8e8697e0eb94e90b455598f
Uzzal-Bhowmik <ubcreativity111@gmail.com>
2025-03-27 16:02:04 +0600

    eslint issues resolved

--- src/app/[restaurantId]/_components/VerifyOtpModal.tsx
+++ src/app/[restaurantId]/_components/VerifyOtpModal.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   AlertDialog,
   AlertDialogAction,
